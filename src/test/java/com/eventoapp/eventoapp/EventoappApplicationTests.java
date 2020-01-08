a9734cd9aaf0674f0096fd10ecd5936b60c7c575
Dannydlira <dannydlira@gmail.com>
2020-01-08 18:31:33 -0300

    Adicionando Projeto Eventos

---
+++ src/test/java/com/eventoapp/eventoapp/EventoappApplicationTests.java
@@ -0,0 +1,12 @@
+package com.eventoapp.eventoapp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoappApplicationTests {
+
+	
+	void contextLoads() {
+	}
+
+}
